c73e601c05b2015399ec6067d755eca08c8ecf66
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2019-05-21 13:09:57 +0900

    openProc用にコードを書き換え

--- src/sketch_12.js
+++ src/sketch_12.js
@@ -28,15 +28,14 @@ let img = new Array(3);
 // バーテックスシェーダ
 // 普通に情報を渡すだけでいい
 let vs =
-"precision mediump float;\
- attribute vec3 aPosition;\
- varying vec2 vTextureCoord;\
- uniform vec2 loc;\
- void main(){\
-   gl_Position = vec4(aPosition, 1.0);\
-   vTextureCoord = vec2((aPosition.x - loc.x) * (384.0 / 200.0), 1.0 - (aPosition.y - loc.y) * (240.0 / 80.0));\
-}\
-";
+"precision mediump float;" +
+"attribute vec3 aPosition;" +
+"varying vec2 vTextureCoord;" +
+"uniform vec2 loc;" +
+"void main(){" +
+"  gl_Position = vec4(aPosition, 1.0);" +
+"  vTextureCoord = vec2((aPosition.x - loc.x) * (384.0 / 200.0), 1.0 - (aPosition.y - loc.y) * (240.0 / 80.0));" +
+"}";
 
 // フラグメントシェーダ
 // 今回はこっちだけいじる。
@@ -52,86 +51,91 @@ let vs =
 // vec3 patternArray: 整数の3つ組[m, n, l]で、lが一番小さい。また、1/m + 1/n + 1/l < PIを満たす。
 // sampler2D button: ボタンのイメージ。
 // float mode: タイリングを描くか、描くとしたらどっちのモードか、あるいはボタンを描くか、の制御。
+
+// mat2 ref0は使ってないので削除
+// 必要なのはm, n, lではなくa, b, s, tなので、a, b, s, tをユニフォームにしてm, n, lは外に出す。
+// m, n, lを決めるのはjs側で行う。
+// さらにいうと、ポアンカレとの切り替えはバーテックスシェーダ側にもモードを用意することで対処できそう。
+// varying使って渡す。その方が正統なんじゃないか。
+// あと、int使った方が礼儀正しいんじゃないかという気もする。intもちゃんと使えるようにしないと。
+
 let fs =
-"precision mediump float;\
- uniform vec2 resolution;\
- uniform float fc;\
- uniform vec3 color_1;\
- uniform vec3 color_2;\
- uniform vec3 patternArray;\
- uniform sampler2D button;\
- varying vec2 vTextureCoord;\
- uniform float mode;\
- const float ITERATIONS = 64.0;\
- const float PI = 3.14159;\
- float calc_ratio(float theta, float phi, float psi){\
-   return sqrt(pow(cos(theta), 2.0) + pow(cos(phi), 2.0) + 2.0 * cos(theta) * cos(phi) * cos(psi)) / sin(psi);\
- }\
- vec2 inversion(vec2 q, float r, float c){\
-   float factor = pow(r, 2.0) / (pow(q.x - c, 2.0) + pow(q.y, 2.0));\
-   return vec2(c, 0.0) + (vec2(q.x - c, q.y) * factor);\
- }\
- vec2 sltf(vec2 z, float a, float b, float c, float d){\
-   vec2 w = vec2((a * d + b * c) * z.x + (a * c) * (z.x * z.x + z.y * z.y) + b * d, z.y);\
-   return w / (pow(c * z.x + d, 2.0) + pow(c * z.y, 2.0));\
- }\
- vec2 poincare_to_half(vec2 p){\
-   float norm = pow(p.x, 2.0) + pow(p.y, 2.0);\
-   return vec2(-2.0 * p.y, (1.0 - norm)) / (1.0 - 2.0 * p.x + norm);\
- }\
- float reflection_3(){\
-   mat2 ref0 = mat2(-1.0, 0.0, 0.0, 1.0);\
-   float time = 300.0 - abs(300.0 - mod(fc, 600.0));\
-   float r = 0.5 + (time * time / 300.0);\
-   float m = patternArray.x;\
-   float n = patternArray.y;\
-   float l = patternArray.z;\
-   float theta = PI / m;\
-   float phi = PI / n;\
-   float psi = PI / l;\
-   float k = calc_ratio(theta, phi, psi);\
-   float y = (k + sqrt(k * k - 1.0)) * r;\
-   float s = k * y / cos(theta);\
-   float t = k * y / cos(phi);\
-   float a = -(cos(phi) + cos(theta) * cos(psi)) / (cos(theta) * sin(psi)) * y;\
-   float b = (cos(theta) + cos(phi) * cos(psi)) / (cos(phi) * sin(psi)) * y;\
-   float count = 0.0;\
-   bool arrived = false;\
-   vec2 p = ((gl_FragCoord.xy + vec2(0.0, -96.0)) * 2.0 - resolution) / min(resolution.x, resolution.y);\
-   if(mode < 0.5){\
-     p = p + vec2(0.0, 1.0);\
-   }else{\
-     if(pow(p.x, 2.0) + pow(p.y, 2.0) > 0.999){ return 2.0; }\
-     p = poincare_to_half(p);\
-   }\
-   float diff = time * PI / 300.0;\
-     p = sltf(p, cos(diff), sin(diff), -sin(diff), cos(diff));\
-     for(float i = 0.0; i < ITERATIONS; i += 1.0){\
-       if(pow(p.x, 2.0) + pow(p.y, 2.0) < r * r){\
-         p = inversion(p, r, 0.0);\
-         count += 1.0;\
-       }else if(pow(p.x - a, 2.0) + pow(p.y, 2.0) > s * s){\
-         p = inversion(p, s, a);\
-         count += 1.0;\
-       }else if(pow(p.x - b, 2.0) + pow(p.y, 2.0) > t * t){\
-         p = inversion(p, t, b);\
-         count += 1.0;\
-       }else{\
-         arrived = true;\
-       }\
-       if(arrived){ break; }\
-     }\
-     return mod(count, 2.0);\
-   }\
- void main(){\
-   if(mode < 1.0){\
-     float ref = reflection_3();\
-     gl_FragColor = vec4((1.0 - step(2.0, ref)) * ((1.0 - ref) * color_1 + ref * color_2), 1.0);\
-   }else{\
-     gl_FragColor = texture2D(button, vTextureCoord);\
-   }\
- }\
-";
+"precision mediump float;" +
+"uniform vec2 resolution;" +
+"uniform float fc;" +
+"uniform vec3 color_1;" +
+"uniform vec3 color_2;" +
+"uniform vec3 patternArray;" +
+"uniform sampler2D button;" +
+"varying vec2 vTextureCoord;" +
+"uniform float mode;" +
+"const float ITERATIONS = 64.0;" +
+"const float PI = 3.14159;" +
+"float calc_ratio(float theta, float phi, float psi){" +
+"  return sqrt(pow(cos(theta), 2.0) + pow(cos(phi), 2.0) + 2.0 * cos(theta) * cos(phi) * cos(psi)) / sin(psi);" +
+"}" +
+"vec2 inversion(vec2 q, float r, float c){" +
+"  float factor = pow(r, 2.0) / (pow(q.x - c, 2.0) + pow(q.y, 2.0));" +
+"  return vec2(c, 0.0) + (vec2(q.x - c, q.y) * factor);" +
+"}" +
+"vec2 sltf(vec2 z, float a, float b, float c, float d){" +
+"  vec2 w = vec2((a * d + b * c) * z.x + (a * c) * (z.x * z.x + z.y * z.y) + b * d, z.y);" +
+"  return w / (pow(c * z.x + d, 2.0) + pow(c * z.y, 2.0));" +
+"}" +
+"vec2 poincare_to_half(vec2 p){" +
+"  return vec2(-2.0 * p.y, (1.0 - pow(p.x, 2.0) - pow(p.y, 2.0))) / (pow(p.x - 1.0, 2.0) + pow(p.y, 2.0));" +
+"}" +
+"float reflection_3(){" +
+"  float time = 300.0 - abs(300.0 - mod(fc, 600.0));" +
+"  float r = 0.5 + (time * time / 300.0);" +
+"  float m = patternArray.x;" +
+"  float n = patternArray.y;" +
+"  float l = patternArray.z;" +
+"  float theta = PI / m;" +
+"  float phi = PI / n;" +
+"  float psi = PI / l;" +
+"  float k = calc_ratio(theta, phi, psi);" +
+"  float y = (k + sqrt(k * k - 1.0)) * r;" +
+"  float s = k * y / cos(theta);" +
+"  float t = k * y / cos(phi);" +
+"  float a = -(cos(phi) + cos(theta) * cos(psi)) / (cos(theta) * sin(psi)) * y;" +
+"  float b = (cos(theta) + cos(phi) * cos(psi)) / (cos(phi) * sin(psi)) * y;" +
+"  float count = 0.0;" +
+"  bool arrived = false;" +
+"  vec2 p = ((gl_FragCoord.xy + vec2(0.0, -96.0)) * 2.0 - resolution) / min(resolution.x, resolution.y);" +
+"  if(mode < 0.5){" +
+"    p = p + vec2(0.0, 1.0);" +
+"  }else{" +
+"    if(pow(p.x, 2.0) + pow(p.y, 2.0) > 0.999){ return 2.0; }" +
+"    p = poincare_to_half(p);" +
+"  }" +
+"  float diff = time * PI / 300.0;" +
+"  p = sltf(p, cos(diff), sin(diff), -sin(diff), cos(diff));" +
+"  for(float i = 0.0; i < ITERATIONS; i += 1.0){" +
+"    if(pow(p.x, 2.0) + pow(p.y, 2.0) < r * r){" +
+"      p = inversion(p, r, 0.0);" +
+"      count += 1.0;" +
+"    }else if(pow(p.x - a, 2.0) + pow(p.y, 2.0) > s * s){" +
+"      p = inversion(p, s, a);" +
+"      count += 1.0;" +
+"    }else if(pow(p.x - b, 2.0) + pow(p.y, 2.0) > t * t){" +
+"      p = inversion(p, t, b);" +
+"      count += 1.0;" +
+"    }else{" +
+"      arrived = true;" +
+"    }" +
+"    if(arrived){ break; }" +
+"  }" +
+"  return mod(count, 2.0);" +
+"}" +
+"void main(){" +
+"  if(mode < 1.0){" +
+"    float ref = reflection_3();" +
+"    gl_FragColor = vec4((1.0 - step(2.0, ref)) * ((1.0 - ref) * color_1 + ref * color_2), 1.0);" +
+"  }else{" +
+"    gl_FragColor = texture2D(button, vTextureCoord);" +
+"  }" +
+"}";
 
 // 0.00, 0.10, 0.17, 0.35, 0.52, 0.64, 0.70, 0.80; ですね。
 
