956c22523e79fbe8314c28904501123793c39f05
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2020-02-25 15:33:10 +0900

    更新

--- src/template.js
+++ src/template.js
@@ -1,4 +1,5 @@
 // 面倒なのでテンプレート作りました
+p5.DisableFriendlyErrors = true;
 'use strict';
 
 let myShader;
@@ -14,13 +15,13 @@ let fs =
 "precision mediump float;" +
 "uniform float fc;" +
 "uniform vec2 resolution;" +
+"uniform vec2 mouse;" +
 "void main(){" +
 "  gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);" +
 "}";
 
 function setup(){
   createCanvas(400, 400, WEBGL);
-  colorMode(HSB, 100);
   noStroke();
   myShader = createShader(vs, fs);
   shader(myShader);
@@ -28,8 +29,9 @@ function setup(){
 }
 
 function draw(){
-  background(70, 30, 100);
+  clear();
   myShader.setUniform("fc", frameCount);
   myShader.setUniform("resolution", [width, height]);
+  myShader.setUniform("mouse", [mouseX, mouseY]);
   box();
 }
